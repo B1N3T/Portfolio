004d96d0638eb7ced817b921d763117623594cf7
JOSE B1N3T <89960004+B1N3T@users.noreply.github.com>
2023-01-18 23:53:16 -0500

    Completed Malloc Practice

--- malloc_prac.c
+++ malloc_prac.c
@@ -3,18 +3,28 @@
 
 int main()
 {
+    int alphLoop = 0;
 	int memory;
 	scanf("%d",&memory);
-	int *ptr = (int *) malloc(memory);
-	char arr[memory];
+	char* arr = (char*) malloc(sizeof(char)*memory);
+    char alph[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 	
 	if(memory>(1024*1024)){
 	    printf("AAA Too Much Memory!!");
+	}else{
+	    for(int i = 0; i < memory; i++){
+            arr[i] = alph[alphLoop];
+            printf("%c",arr[alphLoop]);
+            alphLoop++;
+            if(alphLoop > 25){
+                alphLoop = 0;
+            }
+            if(i >= 400){
+                break;
+            }
+        }
 	}
 	
-	for(int i; i =< sizeof(memory); i++){
-	    arr[*(ptr+i)] 
-	}
-	
+	free(arr);
 	return 0;
 }
