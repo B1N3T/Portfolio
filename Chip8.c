afeb1a835dfc1ea9d6d3ce84ef5198cbffdc3aa3
JOSE B1N3T <89960004+B1N3T@users.noreply.github.com>
2023-01-20 22:35:45 -0500

    Update 2

--- Chip8.c
+++ Chip8.c
@@ -30,10 +30,26 @@ uint16_t opcodes[6] = {
 };
 
 bool Display[64][32];
-uint16_t index_register;
+uint16_t I;
 uint16_t PC;
+uint8_t V0;
+uint8_t V1;
+uint8_t V2;
+uint8_t V3;
+uint8_t V4;
+uint8_t V5;
+uint8_t V6;
+uint8_t V7;
+uint8_t V8;
+uint8_t V9;
+uint8_t VA;
+uint8_t VB;
+uint8_t VC;
+uint8_t VD;
+uint8_t VE;
+uint8_t VF;
 
-uint8_uint16_t PCt CPU()
+uint8_t CPU()
 {
     uint8_t Memory[4096];
     //Font Allocated in Memory
@@ -50,6 +66,7 @@ uint8_uint16_t PCt CPU()
     
     uint8_t delayTimer;
     uint8_t soundTimer;
+    
     return 0;
 };
 
