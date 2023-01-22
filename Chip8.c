13fc888feaa476fa79638e415f9a31b8b9652da2
JOSE B1N3T <89960004+B1N3T@users.noreply.github.com>
2023-01-21 22:26:38 -0500

    Quick Update

--- Chip8.c
+++ Chip8.c
@@ -7,7 +7,7 @@ char keypad{
   return(key);  
 };
 
-const uint8_t FontSet[80] = {
+const char FontSet[80] = {
     //Font
     0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
     0x20, 0x60, 0x20, 0x20, 0x70, // 1
@@ -27,48 +27,32 @@ const uint8_t FontSet[80] = {
     0xF0, 0x80, 0xF0, 0x80, 0x80  // F
 };
 
-uint16_t opcodes[6] = {
+/*uint16_t opcodes[6] = {
     00E0, //Clear screen
     1NNN, //Jump
     6XNN, //Set Register VX
     7XNN, //Add value to register VX
     ANNN, //Set index register
     DXYN  //Display/draw
-};
+};*/
 
+uint16_t opcode;
 bool Display[64][32];
 //Point Counter and Index Register
 uint16_t I;
-uint16_t PC;
+int PC;
 //Registers V0-VF
-uint8_t V0;
-uint8_t V1;
-uint8_t V2;
-uint8_t V3;
-uint8_t V4;
-uint8_t V5;
-uint8_t V6;
-uint8_t V7;
-uint8_t V8;
-uint8_t V9;
-uint8_t VA;
-uint8_t VB;
-uint8_t VC;
-uint8_t VD;
-uint8_t VE;
-uint8_t VF;
+uint8_t V[16];
+//Instruction
+uint16_t inst;
 
 uint8_t CPU()
 {
     uint8_t Memory[4096];
-    //Font Allocated in Memory
-    for(int i = 0; i <= sizeof(FontSet[80]); i++){
-        Memory[50+i] = FontSet[i];
-    }
     
     //Display Set to blank
-    for(int i = 0; i <= 64; i++){
-        for(int j = 0; j <= 32; j++){
+    for(int i = 0; i < 64; i++){
+        for(int j = 0; j < 32; j++){
             Display[i][j] = 0;
         }
     }
@@ -80,8 +64,17 @@ uint8_t CPU()
 };
 
 int main(){
+    //Font Allocated in Memory
+    for(int i = 0; i < sizeof(FontSet[80]); i++){
+        Memory[50+i] = FontSet[i];
+    }
+    
     while(1){
-        code = memory[PC];
+        //Fetch
+        inst = memory[PC] + memory[PC+1];
+        PC += 2;
+        //Decode
+        
     }
     return 0;
 }
